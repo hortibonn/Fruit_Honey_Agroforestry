0fbf029344014542d141dd3d8edd550d18db2aec
Prajna Kasargodu Anebagilu <128603317+PrajnaKARai@users.noreply.github.com>
2025-07-31 12:18:38 +0200

    Funding, Design and Disclaimer added

--- app.R
+++ app.R
@@ -334,6 +334,16 @@ ui <- fluidPage(
               ),
               br(),
               uiOutput("plot8_dl_ui"),
+              tags$img(src = "Funding_declaration.png", height = "100px",
+                       style = "margin-right: auto; max-width: 100%; height: auto; cursor: pointer;"),
+              # tags$h6("Designed and Developed by INRES Horticultural Sciences, University of Bonn"),
+              tags$div(
+                style = "text-align:center;",
+                tags$h6("Designed and Developed by INRES Horticultural Sciences, University of Bonn ",
+                tags$a("Disclaimer", href = "https://agroreforest.eu/reforest-tools-disclaimer/",
+                target = "_blank")
+                        )
+                    )
               br(), br(),br(), br(),
               
     )
