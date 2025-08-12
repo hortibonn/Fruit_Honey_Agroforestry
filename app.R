3c94b94094a732f68932b57e438fcff2ba976051
Prajna Kasargodu Anebagilu <128603317+PrajnaKARai@users.noreply.github.com>
2025-08-12 16:25:54 +0200

    edit

--- app.R
+++ app.R
@@ -344,7 +344,7 @@ ui <- fluidPage(
                 tags$p(
                   tags$a("Disclaimer", href = "https://agroreforest.eu/reforest-tools-disclaimer/",
                 target = "_blank"),
-                     " | "   
+                     " | " ,  
                 tags$a("View Source", href = "https://github.com/hortibonn/Fruit_Honey_Agroforestry/",
                 target = "_blank")
                   ),
